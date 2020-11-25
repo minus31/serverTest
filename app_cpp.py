de47b120ac8fa51001d281fb68d305113676706d
minus31 <n31fromsouth@seerslab.com>
2020-11-25 20:03:37 +0900

    Fix

--- app_cpp.py
+++ app_cpp.py
@@ -69,8 +69,14 @@ def getEvent(sid, data):
         if data["right"]["render"]:
             prevRight = parse_KP(data["right"])
 
+        print(prevLeft)
+        print(prevRight)
+
         prev_info = [prevLeft, prevRight]
         L_KP, R_KP, L_render, R_render = infer_and_estimation(img_parsed, (h, w), prev_info)
+
+        print(L_KP)
+        print(R_KP)
 
         result = {
             "left" : {
