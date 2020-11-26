7e841fb3e966cb6b8bfebaf0b2a7c68ccde1ffa8
minus31 <n31fromsouth@seerslab.com>
2020-11-26 14:46:36 +0900

    Fix

--- cpp_infer.py
+++ cpp_infer.py
@@ -1,7 +1,7 @@
 from pyfootdet import pyFootDetector
 from oneEuroFilter import *
 
-min_cutoff = 1
+min_cutoff = 0.01
 beta = 0
 
 footdetector = pyFootDetector(model_path="./pyfootdet/model/foot_model_encrypted.pb")
